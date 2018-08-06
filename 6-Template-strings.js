e075f6ea028dc0f02bd6779ad3317b5aeb3e995a
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2018-08-07 00:38:51 +0530

    Create 6-Template-strings.js

---
+++ 6-Template-strings.js
@@ -0,0 +1,16 @@
+/*
+Who loves writing large string and variables concatenation? I believe a minority of us. Who hates reading it? I’m sure everyone. 
+Fortunately, ES6 introduces very easy-to-use string templates with placeholders for variables.
+*/
+function hello(firstName, lastName) {
+  return `Good morning ${firstName} ${lastName}! 
+How are you?`
+}
+
+console.log(hello('Jan', 'Kowalski'))
+
+/*
+O/P:
+Good morning Jan Kowalski! 
+How are you?
+*/
